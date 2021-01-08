9fae160a4361e3e2d811cb9d40eac4d93826503b
eshux <54838488+eshux@users.noreply.github.com>
2021-01-08 15:41:54 +0200

    deploy

---
+++ precache-manifest.f6755856ac4f675b201c2b9628b0493a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07d11b4a08e5b02cbfe3",
+    "url": "/mintos/css/app.fb3d5969.css"
+  },
+  {
+    "revision": "743fc052e83b6cab73a0",
+    "url": "/mintos/css/chunk-vendors.b0b28440.css"
+  },
+  {
+    "revision": "891ab755377844ca463b7e0cc740eb40",
+    "url": "/mintos/index.html"
+  },
+  {
+    "revision": "07d11b4a08e5b02cbfe3",
+    "url": "/mintos/js/app.1c0c3c0b.js"
+  },
+  {
+    "revision": "743fc052e83b6cab73a0",
+    "url": "/mintos/js/chunk-vendors.400e1737.js"
+  },
+  {
+    "revision": "2ac64854e0e9bc1c647f0c015bdbcb46",
+    "url": "/mintos/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/mintos/robots.txt"
+  }
+]);
